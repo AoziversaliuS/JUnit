5164ed9eed011063a7f71a2458eddede4bc99352
AoziversaliuS <1220310838@qq.com>
2014-06-07 11:55:29 +0800

    assertThat和hamcrest断言 测试

--- src/junit/OzTest.java
+++ src/junit/OzTest.java
@@ -1,18 +1,32 @@
 package junit;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
 import test.Oz;
-
+import static org.hamcrest.Matchers.*;
 public class OzTest {
 
 	@Test
 	public void testCharToBig() {
-		char X = new Oz().charToBig('a');
-		assertEquals('A', X);
-		assertEquals("不相等", 'A','a');//添加错误提示
+		char X = new Oz().charToBig('b');
+//		assertEquals('A', X);
+//		assertEquals("不相等", 'A','a');//添加错误提示
+		
+		//大于A小于C
+		assertThat(X, allOf(greaterThan('A'),lessThan('C')));
+		
+		//满足其中一个条件
+		assertThat( X, anyOf(greaterThan('A'),lessThan('B')));
+		
+		//等于任何值亦可，null也行
+		assertThat(null, anything());
+		
+		//不等于某个值
+		assertThat(X, not('C'));
+		
+	   //。。。
 	}
 
 }
